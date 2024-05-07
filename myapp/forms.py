55428fa941c7c6fefa00ab021d7d534c928c6fe4
mbrsagor <mbrsagor@gmail.com>
2024-05-07 17:18:16 +0600

    New: styling form and added new cateogry blog table

--- myapp/forms.py
+++ myapp/forms.py
@@ -6,3 +6,11 @@ class BlogModelForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = '__all__'
+        # for styling
+        widgets = {
+            'is_published': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Title'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter content'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+        }
--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     is_published = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='blogs/%Y/%m/%d/', blank=True, null=True)
 
     def __str__(self):
